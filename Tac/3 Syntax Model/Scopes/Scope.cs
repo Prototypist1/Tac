8d7f4564943dc0eea93d7bc7e19921678c059705
Prototypist1 <ColinWielga@outlook.com>
2018-08-16 11:19:27 -0400

    tests pass

--- Tac/3 Syntax Model/Scopes/Scope.cs
+++ Tac/3 Syntax Model/Scopes/Scope.cs
@@ -6,9 +6,11 @@ using Tac.Semantic_Model.Names;
 
 namespace Tac.Semantic_Model
 {
-    // dead end!
     public class RootScope : IScope
     {
+        public override bool Equals(object obj) => obj is RootScope;
+        public override int GetHashCode() => 1522345295;
+
         public bool TryGet<TReferanced>(NamePath key, out TReferanced item) where TReferanced : IReferanced
         {
             item = default;
