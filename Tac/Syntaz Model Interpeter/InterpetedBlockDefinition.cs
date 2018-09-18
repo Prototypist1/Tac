26e788302ea63b5f614ac36d8b7ec5f0aa2e37a4
Prototypist1 <ColinWielga@outlook.com>
2018-09-17 23:38:57 -0400

    🌙

--- Tac/Syntaz Model Interpeter/InterpetedBlockDefinition.cs
+++ Tac/Syntaz Model Interpeter/InterpetedBlockDefinition.cs
@@ -3,6 +3,7 @@ using Prototypist.TaskChain.DataTypes;
 using System.Collections.Generic;
 using Tac.Semantic_Model;
 using Tac.Semantic_Model.CodeStuff;
+using Tac.Semantic_Model.Names;
 
 namespace Tac.Syntaz_Model_Interpeter
 {
