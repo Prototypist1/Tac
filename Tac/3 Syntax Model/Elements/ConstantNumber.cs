097320de1676931c63ac14329e54e777365b7dd9
Prototypist1 <ColinWielga@outlook.com>
2018-09-24 18:36:14 -0400

    slow march

--- Tac/3 Syntax Model/Elements/ConstantNumber.cs
+++ Tac/3 Syntax Model/Elements/ConstantNumber.cs
@@ -1,4 +1,7 @@
-﻿using Tac.Semantic_Model.CodeStuff;
+﻿using System;
+using Tac.New;
+using Tac.Parser;
+using Tac.Semantic_Model.CodeStuff;
 
 namespace Tac.Semantic_Model.Operations
 {
@@ -14,6 +17,65 @@ namespace Tac.Semantic_Model.Operations
         public ITypeDefinition ReturnType(ScopeStack scope)
         {
             return scope.GetType(RootScope.NumberType);
+        }
+    }
+
+    public class ConstantNumberMaker : IMaker<ConstantNumber>
+    {
+        private readonly Func<double, ConstantNumber> make;
+
+        public ConstantNumberMaker(Func<double, ConstantNumber> Make) {
+            make = Make ?? throw new ArgumentNullException(nameof(Make));
+        }
+
+        public bool TryMake(ElementToken elementToken, ElementMatchingContext matchingContext, out IPopulateScope<ConstantNumber> result)
+        {
+            if (TokenMatching.Start(elementToken.Tokens)
+                .Has(ElementMatcher.IsNumber, out double dub)
+                .Has(ElementMatcher.IsDone)
+                .IsMatch)
+            {
+                result = new ConstantNumberPopulateScope(dub, make);
+
+                return true;
+            }
+
+            result = default;
+            return false;
+        }
+
+        private class ConstantNumberPopulateScope : IPopulateScope<ConstantNumber>
+        {
+            private readonly double dub;
+            private readonly Func<double, ConstantNumber> make;
+
+            public ConstantNumberPopulateScope(double dub, Func<double, ConstantNumber> Make)
+            {
+                this.dub = dub;
+                make = Make;
+            }
+
+            public IResolveReferance<ConstantNumber> Run(ScopeTree tree)
+            {
+                return new ConstantNumberResolveReferance(dub,make);
+            }
+        }
+
+        private class ConstantNumberResolveReferance : IResolveReferance<ConstantNumber>
+        {
+            private readonly double dub;
+            private readonly Func<double, ConstantNumber> make;
+
+            public ConstantNumberResolveReferance(double dub, Func<double, ConstantNumber> Make)
+            {
+                this.dub = dub;
+                make = Make;
+            }
+
+            public ConstantNumber Run(ScopeTree tree)
+            {
+                return make(dub);
+            }
         }
     }
 }
