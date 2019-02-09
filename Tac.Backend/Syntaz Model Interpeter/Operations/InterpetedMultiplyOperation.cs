5d091fb26a0bc189ad5f20bc971d34310d5be4b3
Colin <ColinWielga@gmail.com>
2019-02-08 22:37:10 -0500

    thoughts

--- Tac.Backend/Syntaz Model Interpeter/Operations/InterpetedMultiplyOperation.cs
+++ Tac.Backend/Syntaz Model Interpeter/Operations/InterpetedMultiplyOperation.cs
@@ -4,8 +4,16 @@ using Tac.Syntaz_Model_Interpeter.Run_Time_Objects;
 
 namespace Tac.Syntaz_Model_Interpeter
 {
-    internal class InterpetedMultiplyOperation : InterpetedBinaryOperation<IInterpetedNumber,IInterpetedNumber,IInterpetedBoolean>
+    internal class InterpetedMultiplyOperation : InterpetedBinaryOperation<IInterpetedData, IInterpetedData, IInterpetedBoolean>
     {
+        // you are here.
+        // control input types with constuctions
+        // is a interpetedNumber a number member
+        // a interpetedString a string member
+
+        // maybe that would solve my member or value probelm
+        // with value types you would have to copy a lot... any operation would have to save the result in a new
+
         public override IInterpetedResult<IInterpetedBoolean> Interpet(InterpetedContext interpetedContext)
         {
             return InterpetedResult<IInterpetedBoolean>.Create(new RuntimeNumber(
