e193d17cec0792fc6c296463621659ec27141511
Prototypist1 <ColinWielga@outlook.com>
2021-04-25 00:00:18 -0400

    thinking, playing with Blazor

---
+++ Tac.Backend.Emit.2.Test/UnitTest1.cs
@@ -0,0 +1,57 @@
+using System;
+using Tac.Backent.Emit._2;
+using Xunit;
+using Tac.Backend.Emit._2.Test;
+
+
+namespace Tac.Backend.Emit._2.Test
+{
+    public interface ITest
+    {
+
+    }
+
+    public class UnitTest1
+    {
+
+        public partial class Test
+        {
+
+        }
+
+
+
+
+
+        [Fact]
+        public void Test1()
+        {
+            var x = new Test();
+            
+            var y = InteraceEmmiter.EmitOrCast<Test, ITest>(x);
+
+            Assert.NotNull(y);
+        }
+
+
+        [Fact]
+        public void Test2()
+        {
+            var x = new CanI();
+
+            var y = x as ITest;
+
+            Assert.NotNull(y);
+        }
+    }
+}
+
+namespace Tac.Backent.Emit._2
+{
+
+    public partial class CanI: ITest
+    {
+
+    }
+}
+
---
+++ Tac.Web.UI/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tac.Web.UI
+{
+    public class Test
+    {
+        public string Code { get; set; }
+    }
+}
